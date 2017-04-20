1ddf67ec6a02db17b3cb671f8c53b13057f63108
Kimmo Lehto <kimmo.lehto@kontena.io>
2017-04-20 10:11:35 +0300

    Note that it was the AWS cert that failed

--- lib/kontena/machine/aws.rb
+++ lib/kontena/machine/aws.rb
@@ -3,11 +3,11 @@ module Kontena
     module Aws
       def ssl_fail_message(used_option = false)
         if used_option
-          "SSL certificate verify failed.\n" +
+          "AWS SSL certificate verify failed.\n" +
           "You may need to download cacert.pem and set environment variable\n" +
           "SSL_CERT_FILE=/path/to/cacert.pem"
         else
-          "SSL certificate verify failed.\n" +
+          "AWS SSL certificate verify failed.\n" +
           "Try running the command again with option --aws-bundled-cert"
         end
       end
